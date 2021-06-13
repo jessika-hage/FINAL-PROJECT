3905b8d5b066eb643ef44569206c246dd3ed01bf
Ylva Landoff Lindberg <ylva.lan.lin@gmail.com>
2021-06-13 23:21:25 +0200

    fish farm, not done

--- frontend/src/components/map/Farm.js
+++ frontend/src/components/map/Farm.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import { Tooltip } from '@material-ui/core';
 import BugReportIcon from '@material-ui/icons/BugReport';
-
 import { RoomFarm, Icons } from './Styling';
 
 export const Farm = () => {
-  return (
-    <Tooltip title='FARM'>
-      <RoomFarm to='/memorygame'>
-        <Icons>
-          <BugReportIcon fontSize='large' />
-        </Icons>
-      </RoomFarm>
-    </Tooltip>
-  )
+	return (
+		<Tooltip title='PROTIN FARM'>
+			<RoomFarm to='/farm'>
+				<Icons>
+					<BugReportIcon fontSize='large' />
+				</Icons>
+			</RoomFarm>
+		</Tooltip>
+	);
 };
