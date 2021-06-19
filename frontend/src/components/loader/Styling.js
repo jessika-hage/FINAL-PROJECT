bc54183efd720d86c17aa7f981262ba2908b6fef
Ylva Landoff Lindberg <ylva.lan.lin@gmail.com>
2021-06-19 11:12:15 +0200

    added Food and house text,styled  color on loader

--- frontend/src/components/loader/Styling.js
+++ frontend/src/components/loader/Styling.js
@@ -2,7 +2,7 @@ import styled, { keyframes } from 'styled-components';
 
 export const Main = styled.div`
 	align-items: center;
-	background: #1e254a;
+	background-color: ${(props) => props.theme.backgroundColor};
 	display: flex;
 	justify-content: center;
 	margin: 0;
@@ -30,7 +30,6 @@ const Y = keyframes`
   75% {
     transform: translate(0, 50px);
   }
-
 `;
 
 export const Wrapper1 = styled.div`
@@ -55,7 +54,7 @@ export const Wrapper5 = styled.div`
 `;
 
 export const Div1 = styled.div`
-	background: #fff;
+	background: ${(props) => props.theme.textColor};
 	border-radius: 100%;
 	height: 50px;
 	margin: 40px;
@@ -65,7 +64,7 @@ export const Div1 = styled.div`
 `;
 
 export const Div2 = styled.div`
-	background: #fff;
+	background: ${(props) => props.theme.textColor};
 	border-radius: 100%;
 	height: 40px;
 	width: 40px;
@@ -74,7 +73,7 @@ export const Div2 = styled.div`
 `;
 
 export const Div3 = styled.div`
-	background: #fff;
+	background: ${(props) => props.theme.textColor};
 	border-radius: 100%;
 	height: 30px;
 	width: 30px;
@@ -83,7 +82,7 @@ export const Div3 = styled.div`
 `;
 
 export const Div4 = styled.div`
-	background: #fff;
+	background: ${(props) => props.theme.textColor};
 	border-radius: 100%;
 	height: 20px;
 	width: 20px;
@@ -92,80 +91,10 @@ export const Div4 = styled.div`
 `;
 
 export const Div5 = styled.div`
-	background: #fff;
+	background: ${(props) => props.theme.textColor};
 	border-radius: 100%;
 	height: 10px;
 	width: 10px;
 	opacity: 0.2;
 	animation: ${Y} 1.4s linear infinite 0s;
 `;
-
-//   div{
-//     :nth-of-type(5)
-//       height: 10px;
-//       width: 10px;
-//       opacity: 0.2;
-//       animation-delay: ${X} 0.4s;
-//   }
-// `;
-
-// div>div {
-//   background: #fff;
-//   border-radius: 100%;
-//   height: 50px;
-//   margin: 40px;
-//   width: 50px;
-// }
-
-// .wrapper {
-//   position: absolute;
-// }
-
-// .wrapper:nth-of-type(2)>div {
-//     height: 40px;
-//     width: 40px;
-//     opacity: 0.8;
-//   }
-
-//   .wrapper:nth-of-type(3)>div {
-//     height: 30px;
-//     width: 30px;
-//     opacity: 0.6;
-//   }
-
-//   .wrapper:nth-of-type(4)>div {
-//     height: 20px;
-//     width: 20px;
-//     opacity: 0.4;
-//   }
-
-//   .wrapper:nth-of-type(5)>div {
-//     height: 10px;
-//     width: 10px;
-//     opacity: 0.2;
-//   }
-
-// .wrapper {
-//   animation: x 1s ease-in-out alternate infinite 0s both;
-// }
-
-// .wrapper > div {
-//   animation: y 1s linear infinite 0s both;
-// }
-
-// .wrapper:nth-of-type(2),
-// .wrapper:nth-of-type(2) > div {
-//   animation-delay: 0.1s;
-// }
-// .wrapper:nth-of-type(3),
-// .wrapper:nth-of-type(3) > div {
-//   animation-delay: 0.2s;
-// }
-// .wrapper:nth-of-type(4),
-// .wrapper:nth-of-type(4) > div {
-//   animation-delay: 0.3s;
-// }
-// .wrapper:nth-of-type(5),
-// .wrapper:nth-of-type(5) > div {
-//   animation-delay: 0.4s;
-// }
