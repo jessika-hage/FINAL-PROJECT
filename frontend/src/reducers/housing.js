9fd3b44c15f0b4f04663a411f1c3c5f2b8a284a8
Ylva Landoff Lindberg <ylva.lan.lin@gmail.com>
2021-06-19 14:09:39 +0200

    added more houses

--- frontend/src/reducers/housing.js
+++ frontend/src/reducers/housing.js
@@ -19,14 +19,34 @@ const housingData = [
 		ranking: 0.5,
 		image: '1space-5319624_1280.jpg',
 	},
+
 	{
-		id: 5,
+		id: 7,
+		title: 'Red-X Housing',
+		description:
+			' Compact living, with access to everything needed! Commute to the mining site in under 25min.',
+		price: 400000,
+		ranking: 1,
+		image: '4patrick-perkins-DP4dLbZdybA-unsplash.jpg',
+	},
+
+	{
+		id: 6,
 		title: 'Morning Star',
 		description:
 			' Why not setteling down where living is easy! On hovering Morning Star Island you will enjoy a gravity of only 4.807 m/s².',
 		price: 800000,
 		ranking: 1,
 		image: '11fantasy-2700914_1280.jpg',
+	},
+	{
+		id: 5,
+		title: 'Level -15',
+		description:
+			'Private capsuel, with access to cooking facility and decontamination room.',
+		price: 200000,
+		ranking: 1,
+		image: '11capsule-hotel-tokyo-iStock_chee-gin-tan.jpg',
 	},
 ];
 
