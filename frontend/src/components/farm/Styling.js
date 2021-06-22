b910de332174b6295fd4cebb2f0292dede9bd6b8
Ylva Landoff Lindberg <ylva.lan.lin@gmail.com>
2021-06-22 11:28:28 +0200

    new fishgame

--- frontend/src/components/farm/Styling.js
+++ frontend/src/components/farm/Styling.js
@@ -1,43 +1,6 @@
 import styled, { keyframes } from 'styled-components';
 import { FaFish } from 'react-icons/fa';
 
-export const DesktopContainer = styled.div`
-	display: none;
-	@media (min-width: 500px) {
-		display: flex;
-	}
-`;
-
-//Phone wiev
-export const PhoneContainer = styled.div`
-	@media (min-width: 500px) {
-		display: none;
-	}
-`;
-
-export const ShowMobile = styled.div`
-	z-index: 1;
-	position: absolute;
-	top: 300;
-	bottom: 0;
-	width: 100%;
-	height: 50%;
-	background-color: #9de2fa;
-`;
-
-export const BorderContainer = styled.div`
-	bottom: 0;
-	width: 100%;
-	height: 80%;
-	display: flex;
-	flex-direction: column;
-	position: relative;
-	display: flex;
-	justify-content: center;
-	margin: 0 0 0 0;
-	background-color: #dcfcf7;
-`;
-
 const X = keyframes`
   0% {
             transform: translate(-100px, 0);
@@ -59,18 +22,44 @@ const Y = keyframes`
   }
 `;
 
+const Roller = keyframes`
+	0% {
+		transform: translate(200, 0);
+		transform: rotate(-90deg);
+	}
+  10% {
+    transform: translate(-80px, 800);
+  }
+	25% {
+		
+    transform: translate(-200px, -500px);
+		
+  }
+  50% {
+    transform: translate(-500px, 110px);
+		transform: rotate(-180deg);
+  }
+  75% {
+    transform: translate(200, 600px);
+		
+  }
+	100% {
+		transform: translate(50, 0)
+	}
+	;`;
+
 export const RoundWrapper = styled.div`
-	z-index: 3;
-	margin-top: 150px;
+	margin-top: 240px;
 	position: absolute;
 	animation: ${X} 1s ease-in-out alternate infinite 0s both;
+	@media (min-width: 500px) {
+		margin-top: 350px;
+	}
 `;
 export const RoundWrapper2 = styled(RoundWrapper)`
-	z-index: 3;
 	animation: ${X} 2s ease-in-out alternate infinite 0s both;
 `;
 export const RoundWrapper3 = styled(RoundWrapper)`
-	z-index: 3;
 	animation: ${X} 3s ease-in-out alternate infinite 0s both;
 `;
 export const Round1 = styled.div`
@@ -79,44 +68,26 @@ export const Round1 = styled.div`
 	border-radius: 100%;
 	height: 150px;
 	margin: 40px;
-	width: 500px;
+	width: 400px;
 	animation: ${Y} 3s linear infinite 0s;
+	@media (min-width: 500px) {
+		width: 1200px;
+	}
 `;
 export const Round2 = styled(Round1)`
-	animation: ${Y} 3s linear infinite 0s;
+	animation: ${Y} 4s linear infinite 0s;
 	width: 450px;
+	@media (min-width: 500px) {
+		width: 800px;
+	}
 `;
 export const Round3 = styled(Round1)`
 	animation: ${Y} 4s linear infinite 0s;
-	width: 550px;
+	width: 450px;
+	@media (min-width: 500px) {
+		width: 1200px;
+	}
 `;
-
-//fishes
-const Roller = keyframes`
-	0% {
-		transform: translate(50, 0);
-		transform: rotate(-90deg);
-	}
-  10% {
-    transform: translate(-80px, 500);
-  }
-	25% {
-		
-    transform: translate(-200px, -360px);
-		
-  }
-  50% {
-    transform: translate(-400px, 110px);
-		transform: rotate(-180deg);
-  }
-  75% {
-    transform: translate(200, 500px);
-		
-  }
-	100% {
-		transform: translate(50, 0)
-	}
-	;`;
 
 const Roller2 = keyframes`
 	0% {
@@ -142,15 +113,14 @@ const Roller2 = keyframes`
 	;`;
 
 export const FishWrapper1 = styled.button`
-	z-index: 1;
+	z-index: 5;
+	opacity: 0.8;
 	position: absolute;
 	background-color: transparent;
 	right: 20%;
 	top: 70%;
 	left: 80%;
-
-	animation: ${Roller} 11s linear infinite;
-
+	animation: ${Roller} 16s linear infinite;
 	&:disabled {
 		opacity: 0;
 	}
@@ -163,8 +133,9 @@ export const Icon1 = styled(FaFish)`
 
 export const FishWrapper2 = styled.button`
 	background-color: transparent;
-	z-index: 1;
-	animation: ${Roller2} 7s linear infinite;
+	z-index: 5;
+	opacity: 0.8;
+	animation: ${Roller2} 18s linear infinite;
 	top: 50%;
 	right: 50%;
 	left: 50%;
@@ -185,11 +156,12 @@ export const Icon2 = styled(FaFish)`
 
 export const FishWrapper3 = styled.button`
 	background-color: transparent;
-	z-index: 1;
-	animation: ${Roller} 10s linear infinite;
-	top: 80%;
-	right: 20%;
-	left: 80%;
+	z-index: 5;
+	opacity: 0.8;
+	animation: ${Roller} 15s linear infinite;
+	top: 75%;
+	right: 30%;
+	left: 70%;
 	position: absolute;
 	height: 50px;
 	width: 50px;
@@ -205,9 +177,10 @@ export const Icon3 = styled(FaFish)`
 
 export const FishWrapper4 = styled.button`
 	background-color: transparent;
-	z-index: 1;
-	animation: ${Roller} 12s linear infinite;
-	top: 90%;
+	z-index: 5;
+	opacity: 0.8;
+	animation: ${Roller} 17s linear infinite;
+	top: 70%;
 	right: 40%;
 	left: 60%;
 	position: absolute;
@@ -225,8 +198,9 @@ export const Icon4 = styled(FaFish)`
 
 export const FishWrapper5 = styled.button`
 	background-color: transparent;
-	z-index: 1;
-	animation: ${Roller} 7s linear infinite;
+	z-index: 5;
+	opacity: 0.8;
+	animation: ${Roller} 11s linear infinite;
 	top: 70%;
 	right: 40%;
 	left: 60%;
@@ -245,11 +219,12 @@ export const Icon5 = styled(FaFish)`
 
 export const FishWrapper6 = styled.button`
 	background-color: transparent;
-	z-index: 1;
+	z-index: 5;
+	opacity: 0.8;
 	animation: ${Roller} 13s linear infinite;
-	top: 80%;
-	right: 70%;
-	left: 30%;
+	top: 76%;
+	right: 65%;
+	left: 35%;
 	position: absolute;
 	height: 50px;
 	width: 50px;
@@ -265,11 +240,12 @@ export const Icon6 = styled(FaFish)`
 
 export const FishWrapper7 = styled.button`
 	background-color: transparent;
-	z-index: 1;
+	z-index: 5;
+	opacity: 0.8;
 	animation: ${Roller} 15s linear infinite;
-	top: 70%;
-	right: 50%;
-	left: 80%;
+	top: 63%;
+	right: 60%;
+	left: 40%;
 	position: absolute;
 	height: 50px;
 	width: 50px;
@@ -285,7 +261,8 @@ export const Icon7 = styled(FaFish)`
 
 export const FishWrapper8 = styled.button`
 	background-color: transparent;
-	z-index: 1;
+	z-index: 5;
+	opacity: 0.8;
 	animation: ${Roller} 7s linear infinite;
 	top: 80%;
 	right: 50%;
@@ -303,60 +280,10 @@ export const Icon8 = styled(FaFish)`
 
 export const FishWrapper9 = styled.button`
 	background-color: transparent;
-	z-index: 1;
-	animation: ${Roller} 10s linear infinite;
-	top: 90%;
-	right: 50%;
-	left: 80%;
-	position: absolute;
-	&:disabled {
-		opacity: 0;
-	}
-`;
-export const Icon10 = styled(FaFish)`
-	color: #73bed9;
-	opacity: 0.5;
-	font-size: 50px;
-`;
-export const FishWrapper10 = styled.button`
-	background-color: transparent;
-	z-index: 1;
-	animation: ${Roller} 13s linear infinite;
-	top: 90%;
-	right: 50%;
-	left: 80%;
-	position: absolute;
-	&:disabled {
-		opacity: 0;
-	}
-`;
-export const Icon11 = styled(FaFish)`
-	color: #73bed9;
-	opacity: 0.5;
-	font-size: 50px;
-`;
-export const FishWrapper11 = styled.button`
-	background-color: transparent;
-	z-index: 1;
-	animation: ${Roller} 9s linear infinite;
-	top: 80%;
-	right: 30%;
-	left: 70%;
-	position: absolute;
-	&:disabled {
-		opacity: 0;
-	}
-`;
-export const Icon12 = styled(FaFish)`
-	color: #73bed9;
-	opacity: 0.5;
-	font-size: 50px;
-`;
-export const FishWrapper12 = styled.button`
-	background-color: transparent;
-	z-index: 1;
-	animation: ${Roller} 8s linear infinite;
-	top: 70%;
+	z-index: 5;
+	opacity: 0.8;
+	animation: ${Roller} 18s linear infinite;
+	top: 75%;
 	right: 50%;
 	left: 50%;
 	position: absolute;
@@ -370,16 +297,61 @@ export const Icon9 = styled(FaFish)`
 	font-size: 50px;
 `;
 
-export const TransparentWrapper = styled.button`
+export const Icon10 = styled(FaFish)`
+	color: #73bed9;
+	opacity: 0.5;
+	font-size: 50px;
+`;
+export const FishWrapper10 = styled.button`
+	background-color: transparent;
 	z-index: 5;
-	background: transparent;
+	opacity: 0.8;
+	animation: ${Roller} 13s linear infinite;
+	top: 73%;
+	right: 25%;
+	left: 75%;
 	position: absolute;
-	bottom: 0;
-	width: 100%;
-	height: 50%;
+	&:disabled {
+		opacity: 0;
+	}
+`;
+export const Icon11 = styled(FaFish)`
+	color: #73bed9;
+	opacity: 0.5;
+	font-size: 50px;
+`;
+export const FishWrapper11 = styled.button`
+	background-color: transparent;
+	z-index: 5;
+	opacity: 0.8;
+	animation: ${Roller} 9s linear infinite;
+	top: 84%;
+	right: 35%;
+	left: 65%;
+	position: absolute;
+	&:disabled {
+		opacity: 0;
+	}
+`;
+export const Icon12 = styled(FaFish)`
+	color: #73bed9;
+	opacity: 0.5;
+	font-size: 50px;
+`;
+export const FishWrapper12 = styled.button`
+	background-color: transparent;
+	z-index: 5;
+	opacity: 0.8;
+	animation: ${Roller} 8s linear infinite;
+	top: 70%;
+	right: 40%;
+	left: 60%;
+	position: absolute;
+	&:disabled {
+		opacity: 0;
+	}
 `;
 
-//Phone and desktop
 export const GameTitle = styled.h1`
 	z-index: 5;
 	position: absolute;
@@ -402,17 +374,36 @@ export const CounterText = styled(ScoreText)`
 	top: 105px;
 	left: 22px;
 `;
+export const TransparentWrapper = styled.button`
+	z-index: 5;
+	background: transparent;
+	position: absolute;
+	bottom: 0;
+	width: 100%;
+	height: 50%;
+`;
+
+export const BackgroundWater = styled.div`
+	z-index: 1;
+	position: absolute;
+	bottom: 0;
+	width: 100%;
+	height: 50%;
+	background-color: #9de2fa;
+	opacity: 1;
+`;
 
 export const WaterContainer = styled.div`
-	@media (min-width: 500px) {
-		position: relative;
-		display: flex;
-		justify-content: center;
-		margin: 0 0 0 0;
-		background-color: #dcfcf7;
-		width: 800px;
-		height: 600px;
-		border: 10px solid #68ab9e;
+	z-index: 5;
+	position: absolute;
+	bottom: 0;
+	width: 100%;
+	height: 80%;
+	display: flex;
+	justify-content: center;
+	margin: 0 0 0 0;
+	background-color: #dcfcf7;
+	@media (min-width: 768px) {
 	}
 `;
 
@@ -423,6 +414,7 @@ export const MainContainer = styled.main`
 	justify-content: center;
 	align-items: center;
 	position: fixed;
+	padding-top: 40px;
 	background-color: #d1fff4;
 `;
 
